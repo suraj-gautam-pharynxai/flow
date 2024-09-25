2c4ee69702725de53069be0824ef0f5e07886ed8
suraj-gautam-pharynxai <suraj.gautam@pharynxai.com>
2024-09-25 06:32:52 -0400

    files

--- backend/apps/backend/src/app.service.ts
+++ backend/apps/backend/src/app.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
-import validator from 'validator';
+
 @Injectable()
 export class AppService {}
--- backend/libs/repository/src/export.ts
+++ backend/libs/repository/src/export.ts
@@ -1,49 +1,10 @@
-import { ChunkingServiceRepository } from "./services/chunkingService"
-import { RawServiceRepository } from "./services/rawTextService"
-import { BotServiceRepository } from "./services/botService"
-import { ScrapperServiceRepository } from "./services/scrapper"
-import { LlmBotServiceRepository } from "./services/llmBotService"
-import { NlpBotServiceRepository } from "./services/nlpBotService"
-import { EmbeddingServiceRepository } from "./services/embeddingService"
-import { ImageCaptionServiceRepository } from "./services/imageCaptioning"
-import { AuthServiceRepository } from "./services/authService"
-import { PhoneticRepository } from "./services/phoneticService"
-import { PdfParserService } from "./services/parser"
-import { EmbedServiceRepository } from "./services/embedService"
-
-
-
-
 export const Repositories = [
-    ...BotServiceRepository.Repositories,
-    ...ScrapperServiceRepository.Repositories,
-    ...RawServiceRepository.Repositories,
-    ...ChunkingServiceRepository.Repositories,
-    ...EmbeddingServiceRepository.Repositories,
-    ...LlmBotServiceRepository.Repositories,
-    ...NlpBotServiceRepository.Repositories,
-    ...ImageCaptionServiceRepository.Repositories,
-    ...AuthServiceRepository.Repositories,
-    ...PhoneticRepository.Repositories,
-    ...PdfParserService.Repositories,
-    ...EmbedServiceRepository.Repositories
-]
+  // ...BotServiceRepository.Repositories,
+  // ...ScrapperServiceRepository.Repositories,
+  // ...RawServiceRepository.Repositories,
+  // ...ChunkingServiceRepository.Repositories,
+];
 
 export const Entities = [
-    ...RawServiceRepository.Entities,
-    ...BotServiceRepository.Entities,
-    ...ScrapperServiceRepository.Entities,
-    ...ChunkingServiceRepository.Entities,
-    ...LlmBotServiceRepository.Entities,
-    ...NlpBotServiceRepository.Entities,
-    ...EmbeddingServiceRepository.Entities,
-    ...ImageCaptionServiceRepository.Entities,
-    ...AuthServiceRepository.Entities,
-    ...PhoneticRepository.Entities,
-    ...PdfParserService.Entities,
-    ...EmbedServiceRepository.Entities
-]
-
-export const Schemas = [
-    // ...RawServiceRepository.Schemas
-]
+  // ...RawServiceRepository.Entities,
+];
